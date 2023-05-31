6f58b618c46fff84a075cc6dad0cd0d7aaeb7b51
Luciano Robles <54420250+LucianoRobles@users.noreply.github.com>
2023-05-31 18:50:47 -0300

    luhco

--- TpFinal.cpp
+++ TpFinal.cpp
@@ -1,177 +1,367 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <vector>
+#include <sstream>
+//Para borrar podemos usar una estacion predefinida con un codigo que indica que se borro un dato.
+//Busqueda corta porque encontro uno, esta vacio o seguis buscando y llegaste al len.
+
 using namespace std;
 
+int keyTotals = 100; // Tamaño de registros estimados
+int tableLen = 127;  // Tamaño de tabla
 
-string archivoEstaciones="estaciones.txt";
-void leerEstaciones(){
-    ifstream archivo(archivoEstaciones); // Reemplaza "datos.txt" por el nombre de tu archivo
-
-    if (archivo.is_open()) {
-        string linea;
-
-        while (getline(archivo, linea)) {
-            // Procesa la línea leída según tus necesidades
-            cout << linea << endl;
-        }
-
-        archivo.close();
-    } else {
-        cout << "No se pudo abrir el archivo." << endl;
-    }
-}
-
-void buscarEstacionPorCodigo(){
-    ifstream archivo(archivoEstaciones); // Reemplaza "datos.txt" por el nombre de tu archivo
+// Estructura de estaciones
+struct Estaciones
+{
     string codigo;
-    cout<<"Ingrese el codigo de la estacion que desea buscar: "<<endl;
-    cin>>codigo;
+    string nombre;
+    string ciudad;
+    int cantSurtidores;
+    double litrosSurtidor;
+    string tipoCombustible;
+    //bool colicione;
+};
 
-    if (archivo.is_open()) {
-        string linea;
-        int nroLinea=0;
-        bool encontrado=false;
+string archivoEstaciones = "estaciones.txt"; // Archivo incial de estaciones
+vector<Estaciones> tablaHashing(tableLen);     // Tabla de hashing
 
-        while (getline(archivo, linea) && !encontrado) {
-            // Procesa la línea leída según tus necesidades
-            nroLinea++;
-            if (linea.find(codigo) != string::npos) {
-                cout << "Elemento encontrado en la línea " << codigo << ": " << linea <<endl;
-                encontrado=true;
-                // Puedes realizar cualquier acción adicional que desees cuando encuentres el elemento
-            }
-        }
-        if (!encontrado){
-            cout<<"La estacion ingresada no es valida"<<endl;
-        }
-        
-
-        archivo.close();
-    } else {
-        cout << "No se pudo abrir el archivo." << endl;
-    }
+//Funciones auxiliares
+bool verificarTipoComb (string tipo) {
+    return tipo == "SUP" || tipo == "INF" || tipo == "NIT";
 }
-void eliminarEstacionPorCodigo(){
+
+// Funciones de hashing
+// Hashing Modulo
+int functionModulo(int key, int tamanio)
+{
+    return key % tamanio;
+}
+
+// Hashing Folding ultimos 3 + Divicion
+// Devuele posicion
+
+int functionDeHashing(string codigo, int tamanio)
+{
+    int salida = 0, posicion;
+    for (int i = codigo.size() - 3; i < codigo.size(); i++)
+    {
+        salida += codigo[i];
+        // cout <<"Salida= "<< salida <<"Codigo= "<< codigo[i] << endl;
+    }
+    posicion = functionModulo(salida, tamanio);
+    return posicion;
+}
+
+bool noEstaOcupado(int posicion)
+{
+    return tablaHashing[posicion].codigo.empty();
+}
+
+// Colicion Quadratic probing
+// Si da -1 no encontro posicion
+int functionHashingCollision(int posicion, int tamanio)
+{
+    int posicionAux, salida = -1;
+    for (int i = 1; i < tamanio; i++)
+    {
+        posicionAux = posicion + (i * i);
+        if (noEstaOcupado(posicionAux))
+        {
+            salida = posicionAux;
+            // tablaHashing.insert(tablaHashing.begin()+posicionAux,estacion);
+            break;
+        }
+    }
+    return salida;
+}
+
+// Insercion
+void funtionDeInsertion(Estaciones estacion, int tamanio)
+{
+    int posicionColision, posicionInicial = functionDeHashing(estacion.codigo, tamanio);
+    if (noEstaOcupado(posicionInicial))
+    {
+        //Sumar el codigo agregado al txt
+        tablaHashing.insert(tablaHashing.begin() + posicionInicial, estacion);
+        cout << "Soy la estacion: " << estacion.nombre << "Entre en pos Inicial: " << "Pos: " << posicionInicial << endl;
+    }
+    else
+    {
+        posicionColision = functionHashingCollision(posicionInicial, tamanio);
+        if (posicionColision != (-1))
+        {
+            //Sumar el codigo agregado al txt
+            tablaHashing.insert(tablaHashing.begin() + posicionColision, estacion);
+            cout << "Soy la estacion: " << estacion.nombre << " Entre en pos Colision: " << "PosCos: " << posicionColision << endl;
+        }
+        else
+        {
+            cout << "No se pudo insertar la estacion en ningun lugar" << endl;
+        }
+    }
+    cout << "Estaciones registrada" << endl;
+}
+
+// Cargar estaciones desde el txt.
+void cargarEstacionesIniciales()
+{
     ifstream archivo(archivoEstaciones); // Reemplaza "datos.txt" por el nombre de tu archivo
-    bool encontrado=false;
-    string codigo;
-    cout<<"Ingrese el codigo de la estacion a eliminar: "<<endl;
-    cin>>codigo;
-    if (archivo.is_open()) {
+
+    if (archivo.is_open())
+    {
         string linea;
-        int nroLinea=0;
 
-        while (getline(archivo, linea) && !encontrado) {
-            // Procesa la línea leída según tus necesidades
-            nroLinea++;
-            if (linea.find(codigo) != string::npos) {
-                cout << "Elemento encontrado en la línea " << codigo << ": " << linea << std::endl;
-                encontrado=true;
-                // Puedes realizar cualquier acción adicional que desees cuando encuentres el elemento
+        while (getline(archivo, linea))
+        {
+
+            istringstream iss(linea);
+            string codigo;
+            string nombre;
+            string ciudad;
+            int cantSurtidores;
+            double litrosSurtidor;
+            string tipoCombustible;
+
+            Estaciones estacionNueva;
+
+            if (iss >> codigo >> nombre >> ciudad >> cantSurtidores >> litrosSurtidor >> tipoCombustible)
+            {
+                estacionNueva.codigo = codigo;
+                estacionNueva.nombre = nombre;
+                estacionNueva.ciudad = ciudad;
+                estacionNueva.cantSurtidores = cantSurtidores;
+                estacionNueva.litrosSurtidor = litrosSurtidor;
+                estacionNueva.tipoCombustible = tipoCombustible;
             }
+            funtionDeInsertion(estacionNueva, tableLen);
         }
-
         archivo.close();
-    } else {
+    }
+    else
+    {
         cout << "No se pudo abrir el archivo." << endl;
     }
 }
-bool encontrarEstacion(string codigo){
-    ifstream archivo(archivoEstaciones);
-    bool encontrado=false;
 
-    if (archivo.is_open()) {
-        string linea;
-        int nroLinea=0;
-        while (getline(archivo, linea)) {
-            nroLinea++;
-            if (linea.find(codigo) != string::npos) {
-                encontrado=true;
-            }
-            
+//Dar de alta acomodar con hashing
+void darDeAltaEstacionV2(){
+    string codigo,nombre,ciudad,tipoCombustible;
+    int cantSurtidores,litrosSurtidor;
+    Estaciones estacionNueva;
     
+    cout << "Ingrese el CODIGO de 6 digitos de la estacion nueva: " << endl;
+    cin >> codigo;
+    while (codigo.size() == 6) {
+        cout << "El CODIGO debe ser de 6 digitos ej: ABC123, vuelve a intentarlo: " << endl;
+        cin >> codigo;
+    }
+    cout << "Ingrese el nombre: " << endl;
+    cin >> nombre;
+    cout << "Ingrese la ciudad: " << endl;
+    cin >> ciudad;
+    cout << "Ingrese el tipo de combustible: " << endl;
+    cin >> tipoCombustible;
+    while (not verificarTipoComb(tipoCombustible)) {
+        cout << "Los tipos de combustible validos son SUP-INF-NIT : " << endl;
+        cin >> tipoCombustible;
+    }
+    cout << "Ingrese la cantidad de surtidores: " << endl;
+    cin >> cantSurtidores;
+    cout << "Ingrese los litros que tiene cada surtidor: " << endl;
+    cin >> litrosSurtidor;
+    estacionNueva.codigo = codigo;
+    estacionNueva.nombre = nombre;
+    estacionNueva.ciudad = ciudad;
+    estacionNueva.cantSurtidores = cantSurtidores;
+    estacionNueva.litrosSurtidor = litrosSurtidor;
+    estacionNueva.tipoCombustible = tipoCombustible;
+    cout << "Todos los datos fueron ingresados correctamente" << endl;
+
+    //Insercion
+    funtionDeInsertion(estacionNueva, tableLen);
+}
+
+void buscarEstacionPorCodigo()
+{
+    ifstream archivo(archivoEstaciones); // Reemplaza "datos.txt" por el nombre de tu archivo
+    string codigo;
+    cout << "Ingrese el codigo de la estacion que desea buscar: " << endl;
+    cin >> codigo;
+
+    if (archivo.is_open())
+    {
+        string linea;
+        int nroLinea = 0;
+        bool encontrado = false;
+
+        while (getline(archivo, linea) && !encontrado)
+        {
+            // Procesa la línea leída según tus necesidades
+            nroLinea++;
+            if (linea.find(codigo) != string::npos)
+            {
+                cout << "Elemento encontrado en la línea " << codigo << ": " << linea << endl;
+                encontrado = true;
+                // Puedes realizar cualquier acción adicional que desees cuando encuentres el elemento
+            }
+        }
+        if (!encontrado)
+        {
+            cout << "La estacion ingresada no es valida" << endl;
+        }
+
+        archivo.close();
+    }
+    else
+    {
+        cout << "No se pudo abrir el archivo." << endl;
+    }
+}
+
+void eliminarEstacionPorCodigo()
+{
+    ifstream archivo(archivoEstaciones); // Reemplaza "datos.txt" por el nombre de tu archivo
+    bool encontrado = false;
+    string codigo;
+    cout << "Ingrese el codigo de la estacion a eliminar: " << endl;
+    cin >> codigo;
+    if (archivo.is_open())
+    {
+        string linea;
+        int nroLinea = 0;
+
+        while (getline(archivo, linea) && !encontrado)
+        {
+            // Procesa la línea leída según tus necesidades
+            nroLinea++;
+            if (linea.find(codigo) != string::npos)
+            {
+                cout << "Elemento encontrado en la línea " << codigo << ": " << linea << std::endl;
+                encontrado = true;
+                // Puedes realizar cualquier acción adicional que desees cuando encuentres el elemento
+            }
+        }
+
+        archivo.close();
+    }
+    else
+    {
+        cout << "No se pudo abrir el archivo." << endl;
+    }
+}
+bool encontrarEstacion(string codigo)
+{
+    ifstream archivo(archivoEstaciones);
+    bool encontrado = false;
+
+    if (archivo.is_open())
+    {
+        string linea;
+        int nroLinea = 0;
+        while (getline(archivo, linea))
+        {
+            nroLinea++;
+            if (linea.find(codigo) != string::npos)
+            {
+                encontrado = true;
+            }
         }
         archivo.close();
-    }else{
-        cout<<"no se puede abrir"<<endl;
+    }
+    else
+    {
+        cout << "no se puede abrir" << endl;
     }
     return encontrado;
 }
 
-
-
-
-void darDeAltaEstacion() {
-    ofstream archivo(archivoEstaciones,ios::app); // Reemplaza "datos.txt" por el nombre de tu archivo
-    bool encontrado=false;
-    if (archivo.is_open()) {
-        string codigo,ciudad,datos;
-        cout<<"Ingrese el codigo de la estacion: "<<endl;
-        cin>>codigo;
-        while(encontrarEstacion(codigo)){
-            cout<<"El codigo ingresado ya existe\nIngresa otro codigo: "<<endl;
-            cin>>codigo;
+void darDeAltaEstacion()
+{
+    ofstream archivo(archivoEstaciones, ios::app); // Reemplaza "datos.txt" por el nombre de tu archivo
+    bool encontrado = false;
+    if (archivo.is_open())
+    {
+        string codigo, ciudad, datos;
+        cout << "Ingrese el codigo de la estacion: " << endl;
+        cin >> codigo;
+        while (encontrarEstacion(codigo))
+        {
+            cout << "El codigo ingresado ya existe\nIngresa otro codigo: " << endl;
+            cin >> codigo;
         }
-        cout<<"Ingrese la ciudad de la estacion: "<<endl;
-        cin>>ciudad;
-        datos= codigo +" "+ ciudad;
-        archivo << datos <<endl;
+        cout << "Ingrese la ciudad de la estacion: " << endl;
+        cin >> ciudad;
+        datos = codigo + " " + ciudad;
+        archivo << datos << endl;
         archivo.close();
-        cout << "Datos escritos en el archivo correctamente." <<endl;
-    } else {
-        cout << "No se pudo abrir el archivo." <<endl;
+        cout << "Datos escritos en el archivo correctamente." << endl;
     }
-    
+    else
+    {
+        cout << "No se pudo abrir el archivo." << endl;
+    }
 }
-char deseaContinuar(){
+char deseaContinuar()
+{
     char dato;
-    cout<<"Si desea continuar ingrese s: ";
+    cout << "Si desea continuar ingrese s: ";
     cin >> dato;
-    if(dato=='s'|| dato=='S'){
+    if (dato == 's' || dato == 'S')
+    {
         return dato;
-    }else{
+    }
+    else
+    {
         return 'n';
     }
-    
 }
 
-//Agregar bucle para salida tipo bot.
-void mostrarMenu(){
+// Agregar bucle para salida tipo bot.
+void mostrarMenu()
+{
     char opc;
-    cout<<"Bienvenido al Menu de opciones,elija la opcion: "<<endl;
-    
-    while(opc!='n'){
-        cout<<"1-Consultar informacion de una estacion en particular \n2-Dar de alta una nueva estacion\n3-Dar de baja estaciones\n4-Mostrar todas las estaciones\n5-Salir"<<endl;
-        cin>>opc;
-        if(opc=='1'){
-        buscarEstacionPorCodigo();
-        opc=deseaContinuar();
-        }else if(opc=='2'){
-            darDeAltaEstacion();
-            opc=deseaContinuar();  
-        }else if (opc=='3'){
+    cout << "Bienvenido al Menu de opciones, elija la opcion: " << endl;
+
+    while (opc != 'n')
+    {
+        cout << "1-Consultar informacion de una estacion en particular \n2-Dar de alta una nueva estacion\n3-Dar de baja estaciones\n4-Mostrar todas las estaciones\n5-Salir" << endl;
+        cin >> opc;
+        if (opc == '1')
+        {
+            buscarEstacionPorCodigo();
+            opc = deseaContinuar();
+        }
+        else if (opc == '2')
+        {
+            darDeAltaEstacionV2();
+            opc = deseaContinuar();
+        }
+        else if (opc == '3')
+        {
             eliminarEstacionPorCodigo();
-            opc=deseaContinuar();
-            
+            opc = deseaContinuar();
         }
     }
-    cout<<"Gracias por utilizar nuestro sistema!"<<endl;
+    cout << "Gracias por utilizar nuestro sistema!" << endl;
 }
-    
-void deseaSeguir(){
+
+void deseaSeguir()
+{
     char resp;
-    cout<<"Desea continuar? s/n"<<endl;
-    cin>>resp;
-    if(resp=='s'|| resp=='S'){
+    cout << "Desea continuar? s/n" << endl;
+    cin >> resp;
+    if (resp == 's' || resp == 'S')
+    {
         mostrarMenu();
     }
-    cout<<"Nos vemo nemo"<<endl;
+    cout << "Nos vemo nemo" << endl;
 }
 
-int main() {
-    mostrarMenu();
-    //leerEstaciones();
+int main()
+{   
+    cargarEstacionesIniciales();
+    //mostrarMenu();
+    // leerEstaciones();
     return 0;
 }
-
